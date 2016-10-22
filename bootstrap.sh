ca4c1a869467dbe09e19df899e94107d3671ade1
Mateusz Tomczyk <tulfm@poczta.fm>
2016-10-22 14:13:04 +0200

    tulos

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+pacman -S git
+cd ~
+git clone https://github.com/tulustul/TulOS.git & cd TulOS
+
+./configure
+
---
+++ configure.sh
@@ -0,0 +1,10 @@
+pacman -Syu < pacman-packages
+
+pip install virtualenvwrapper
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+cp -R home/. ~/
+
+touch ~/.tulrc
+
