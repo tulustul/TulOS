7e37a8f273220163811379ac367efc9df2271e20
Mateusz Tomczyk <tulfm@poczta.fm>
2016-10-22 18:09:27 +0200

    tulos

--- configure.sh
+++ configure.sh
@@ -1,4 +1,13 @@
-pacman -Syu - < pacman-packages --noconfirm
+git clone https://aur.archlinux.org/package-query.git
+cd package-query
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt
+makepkg -si
+cd ..
+
+yaourt -Syu - < packages --noconfirm
 
 pip install virtualenvwrapper
 
