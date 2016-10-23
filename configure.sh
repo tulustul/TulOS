ab5f5b6ee68a71ce92c74f176b087540a9ca964c
Mateusz Tomczyk <tulfm@poczta.fm>
2016-10-23 11:25:13 +0200

    tulos

--- configure.sh
+++ configure.sh
@@ -1,14 +1,14 @@
 set -e
 
 function install_aur_package {
-    if (pacman -Q package-query >/dev/null) then
+    if ! (pacman -Q package-query >/dev/null) then
         yaourt -S $1 --noconfirm
     fi
 }
 
 sudo pacman -Syu - < packages --noconfirm
 
-if (pacman -Q package-query >/dev/null) then
+if ! (pacman -Q package-query >/dev/null) then
     cd ~
     rm -Rf package-query
     git clone https://aur.archlinux.org/package-query.git
@@ -17,7 +17,7 @@ if (pacman -Q package-query >/dev/null) then
     rm -Rf package-query
 fi
 
-if (pacman -Q yaourt >/dev/null) then
+if ! (pacman -Q yaourt >/dev/null) then
     cd ~
     rm -Rf yaourt
     git clone https://aur.archlinux.org/yaourt.git
